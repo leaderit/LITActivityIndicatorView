37a513e83463d5bd51577850540e8da458e04e55
Valerii Grazhdankin <ias-projects@mail.ru>
2020-11-30 13:54:52 +0500

    Initial Commit

---
+++ LITActivityIndicator/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LITActivityIndicator
+//
+//  Created by vg on 11/30/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ LITActivityIndicator/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  LITActivityIndicator
+//
+//  Created by vg on 11/30/20.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
