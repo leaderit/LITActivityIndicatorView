f6fb175a5c5e2cb109bb8cbc1783e64e6f611c6e
Valerii Grazhdankin <ias-projects@mail.ru>
2020-11-30 19:28:34 +0500

    Add new one

---
+++ LITActivityIndicator/LITActivityIndicator/LITActivityIndicator.h
@@ -0,0 +1,25 @@
+//
+//  LITActivityIndicator.h
+//  Leader IT Extended Activity Indicator
+//
+//  Created by Valerii Grazhdankin on 11/28/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+IB_DESIGNABLE
+@interface LITActivityIndicator : UIActivityIndicatorView
+
+@property (nonatomic, strong) IBInspectable UIImage * image;
+@property (nonatomic, strong) IBInspectable UIImage * staticImage;
+@property (nonatomic, assign) IBInspectable int steps;
+@property (nonatomic, assign) IBInspectable double duration;
+
+-(id)initWithImage:( UIImage* ) image;
+-(id)initWithImages:( UIImage* ) image staticImage:( UIImage * _Nullable ) staticImage;
+
+@end
+
+NS_ASSUME_NONNULL_END
